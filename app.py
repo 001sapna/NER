553b9c63f5b5ec3c14f8297aa17b7058e4d27699
001sapna <1sapnakumari.28@gmail.com>
2024-07-07 21:30:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ except OSError:
 # Load your NER model
 @st.cache_resource
 def load_ner_model():
-    return load_model('NERR.keras')
+    return load_model('ner.keras')
 
 model = load_ner_model()
 
